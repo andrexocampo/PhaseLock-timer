f0ff5953263c21d9e01d4124b9932e6bdf90b9d5
andrexocampo <andresocampo0412@gmail.com>
2025-12-16 12:20:36 -0500

    Initial commit: Spring Boot PhaseLock Timer application

---
+++ src/main/java/com/portafolio/PhaseLock_timer/PhaseLockTimerApplication.java
@@ -0,0 +1,13 @@
+package com.portafolio.PhaseLock_timer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhaseLockTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhaseLockTimerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portafolio/PhaseLock_timer/PhaseLockTimerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.PhaseLock_timer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhaseLockTimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
